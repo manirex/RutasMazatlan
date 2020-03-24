dc4f023d15afd1fad586a8a3c482854465f0cf20
ivanmastercui <62372025+ivanmastercui@users.noreply.github.com>
2020-03-23 19:58:56 -0700

    manifeo

--- app/src/main/java/com/example/rutasmazatlan/SplashActivity.java
+++ app/src/main/java/com/example/rutasmazatlan/SplashActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
@@ -20,6 +23,14 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        if (Build.VERSION.SDK_INT >= 16) { //CHEQUEO DE LA API
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  // FULL SCREEN
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION                 // QUITAR LA  NAVEGACION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
 /////////////////////////////////////LINEA FUENTES ////////////////////////////////////////////////////////////
         String fuente1 = "fonts/RedRock.ttf";
         this.RedRockRuta = Typeface.createFromAsset(getAssets(), fuente1);
